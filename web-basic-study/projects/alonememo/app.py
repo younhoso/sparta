eacb77e693bcb27be6838e48e5bc8458168e3848
younhoso <thdbsgh3443@naver.com>
2022-02-15 11:14:37 +0900

    5주차 무비스타 시작

--- web-basic-study/projects/alonememo/app.py
+++ web-basic-study/projects/alonememo/app.py
@@ -15,9 +15,8 @@ def home():
 
 @app.route('/memo', methods=['GET'])
 def listing():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg':'GET 연결되었습니다!'})
+    articles = list(db.articles.find({}, {'_id':False}))
+    return jsonify({'all_articles':articles})
 
 ## API 역할을 하는 부분
 @app.route('/memo', methods=['POST'])
@@ -41,4 +40,4 @@ def saving():
     return jsonify({'msg':'저장이 완료되었습니다!'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0',port=5001,debug=True)
