d4f36be53038e86e34fbea563f8dba484f94e4dc
younhoso <thdbsgh3443@naver.com>
2022-02-15 10:21:02 +0900

    4주차[나홀로메모장] - POST 연습(메모하기)

--- web-basic-study/projects/alonememo/app.py
+++ web-basic-study/projects/alonememo/app.py
@@ -22,9 +22,23 @@ def listing():
 ## API 역할을 하는 부분
 @app.route('/memo', methods=['POST'])
 def saving():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg':'POST 연결되었습니다!'})
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    # 여기에 코딩을 해서 meta tag를 먼저 가져와보겠습니다.
+    title = soup.select_one('meta[property="og:title"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+
+    doc = {'title':title, 'image':image, 'desc':desc, 'url': url_receive, 'comment':comment_receive}
+    db.articles.insert_one(doc)
+
+    return jsonify({'msg':'저장이 완료되었습니다!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
