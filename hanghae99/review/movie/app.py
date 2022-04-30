4f75c645f81c92eab4b7395fa9f7f16a963aec99
younhoso <thdbsgh3443@naver.com>
2022-04-30 09:11:14 +0900

    homework 작업

---
+++ hanghae99/review/movie/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+import certifi #보안 이슈가 있을경우 설치해야 하는 플러그인
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://soyounho:syh5142024@cluster0.3epib.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca) # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta # 'DB이름'라는 이름의 db를 만듭니다.
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/movie", methods=["POST"])
+def movie_post():
+		url_receive = request.form['url_give']
+		star_receive = request.form['star_give']
+		comment_receive = request.form['comment_give']
+
+		headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+		data = requests.get(url_receive, headers=headers)
+
+		soup = BeautifulSoup(data.text, 'html.parser')
+
+		title = soup.select_one('meta[property="og:title"]')['content']
+		image = soup.select_one('meta[property="og:image"]')['content']
+		desc = soup.select_one('meta[property="og:description"]')['content']
+
+		doc = {
+			'title': title,
+			'image': image,
+			'desc': desc,
+			'star': star_receive,
+			'comment': comment_receive
+		}
+		db.movies.insert_one(doc)
+		
+		return jsonify({'msg':'저장 완료!'})
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+		movie_list = list(db.movies.find({}, {'_id':False}))
+		return jsonify({'movies':movie_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
