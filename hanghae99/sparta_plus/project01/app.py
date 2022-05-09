a8b22001f11cde4c5c2e5c13466307156e5d0430
younhoso <thdbsgh3443@naver.com>
2022-05-09 20:40:23 +0900

    나홀로일기장 Lv.1 만들기(API)

--- hanghae99/sparta_plus/project01/app.py
+++ hanghae99/sparta_plus/project01/app.py
@@ -1,21 +1,30 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta_plus_week1
+
 @app.route('/')
 def home():
    return render_template('index.html')
 
 @app.route('/diary', methods=['GET'])
 def show_diary():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg': 'GET 연결 완료!'})
+    diaries = list(db.diary.find({}, {'_id': False}))
+    return jsonify({'all_diary': diaries})
 
 @app.route('/diary', methods=['POST'])
 def save_diary():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 요청 완료!'})
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+
+    doc = {
+        'title': title_receive,
+        'content': content_receive
+    }
+    db.diary.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
