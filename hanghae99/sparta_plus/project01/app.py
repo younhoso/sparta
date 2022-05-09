c97c86af0a4144273a003ad1f406922a1fba350a
younhoso <thdbsgh3443@naver.com>
2022-05-09 20:07:03 +0900

    나홀로일기장 Lv.1 만들기(연결)

--- hanghae99/sparta_plus/project01/app.py
+++ hanghae99/sparta_plus/project01/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'This is Home!'
+   return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    sample_receive = request.args.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg': 'GET 연결 완료!'})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'POST 요청 완료!'})
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
