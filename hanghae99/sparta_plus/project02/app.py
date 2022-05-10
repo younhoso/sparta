98f970f96b777057a5fdd69df53ede65b223a0c4
younhoso <thdbsgh3443@naver.com>
2022-05-10 16:35:29 +0900

    상세 페이지 - jinja2로 뜻 가져오기

--- hanghae99/sparta_plus/project02/app.py
+++ hanghae99/sparta_plus/project02/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, redirect, url_for
 from pymongo import MongoClient
+import requests
 
 app = Flask(__name__)
 
@@ -16,7 +17,9 @@ def main():
 @app.route('/detail/<keyword>')
 def detail(keyword):
     # API에서 단어 뜻 찾아서 결과 보내기
-    return render_template("detail.html", word=keyword)
+    r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token 638fec2a658eb48efcc8f3c5072498878c0432b0"})
+    result = r.json()
+    return render_template("detail.html", word=keyword, result=result)
 
 
 @app.route('/api/save_word', methods=['POST'])
