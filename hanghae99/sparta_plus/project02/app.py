9ad2e29ab57b91ccf359bc22e7f3b1422228bfc8
younhoso <thdbsgh3443@naver.com>
2022-05-10 11:58:49 +0900

    플라스크로 멀티페이지 사이트 만들기 - 기초

---
+++ hanghae99/sparta_plus/project02/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    myname = "Sparta"
+    return render_template("index.html", name=myname)
+
+
+@app.route('/detail')
+def detail():
+    return render_template("detail.html")
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
