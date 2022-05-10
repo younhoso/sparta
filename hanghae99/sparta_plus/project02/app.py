0008dcef1f41b003f3c6e81d439d2cdda62d50b0
younhoso <thdbsgh3443@naver.com>
2022-05-10 22:35:10 +0900

    2주차 숙제 완료

--- hanghae99/sparta_plus/project02/app.py
+++ hanghae99/sparta_plus/project02/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 import requests
 
@@ -10,36 +10,71 @@ db = client.dbsparta_plus_week2
 
 @app.route('/')
 def main():
+    msg = request.args.get("msg")
+
     # DB에서 저장된 단어 찾아서 HTML에 나타내기
     words = list(db.words.find({}, {"_id": False}))
-    return render_template("index.html", words=words)
+    return render_template("index.html", words=words, msg=msg)
 
 
 @app.route('/detail/<keyword>')
 def detail(keyword):
-    status_receive = request.args.get("status_give")
     # API에서 단어 뜻 찾아서 결과 보내기
-    r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token 638fec2a658eb48efcc8f3c5072498878c0432b0"})
+    status_receive = request.args.get("status_give", "old")
+    r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}",
+                     headers={"Authorization": "Token 638fec2a658eb48efcc8f3c5072498878c0432b0"})
+    if r.status_code != 200:
+        return redirect(url_for("main", msg="Word not found in dictionary; Try another word"))
     result = r.json()
+    print(result)
     return render_template("detail.html", word=keyword, result=result, status=status_receive)
 
 
 @app.route('/api/save_word', methods=['POST'])
 def save_word():
     # 단어 저장하기
-    word_receive = request.form["word_give"]
-    definition_receive = request.form["definition_give"]
+    word_receive = request.form['word_give']
+    definition_receive = request.form['definition_give']
     doc = {"word": word_receive, "definition": definition_receive}
     db.words.insert_one(doc)
-    return jsonify({'result': 'success', 'msg': f'단어 {word_receive}저장'})
+    return jsonify({'result': 'success', 'msg': f'word "{word_receive}" saved'})
 
 
 @app.route('/api/delete_word', methods=['POST'])
 def delete_word():
     # 단어 삭제하기
-    word_receive = request.form["word_give"]
+    word_receive = request.form['word_give']
     db.words.delete_one({"word": word_receive})
-    return jsonify({'result': 'success', 'msg': f'단어 {word_receive}삭제!'})
+    db.examples.delete_many({"word": word_receive})
+    return jsonify({'result': 'success', 'msg': f'word "{word_receive}" deleted'})
+
+
+@app.route('/api/get_examples', methods=['GET'])
+def get_exs():
+    word_receive = request.args.get("word_give")
+    result = list(db.examples.find({"word": word_receive}, {'_id': 0}))
+    print(word_receive, len(result))
+
+    return jsonify({'result': 'success', 'examples': result})
+
+
+@app.route('/api/save_ex', methods=['POST'])
+def save_ex():
+    word_receive = request.form['word_give']
+    example_receive = request.form['example_give']
+    doc = {"word": word_receive, "example": example_receive}
+    db.examples.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': f'example "{example_receive}" saved'})
+
+
+@app.route('/api/delete_ex', methods=['POST'])
+def delete_ex():
+    word_receive = request.form['word_give']
+    number_receive = int(request.form["number_give"])
+    example = list(db.examples.find({"word": word_receive}))[number_receive]["example"]
+    print(word_receive, example)
+    db.examples.delete_one({"word": word_receive, "example": example})
+    return jsonify({'result': 'success', 'msg': f'example #{number_receive} of "{word_receive}" deleted'})
 
 
 if __name__ == '__main__':
