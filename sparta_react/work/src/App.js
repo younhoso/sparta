fc52e027d8a9c00acfcfaab73704fb4536797b25
younhoso <thdbsgh3443@naver.com>
2022-05-23 09:51:24 +0900

    route ex

--- sparta_react/work/src/App.js
+++ sparta_react/work/src/App.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React,{useState, useRef} from "react";
 import Start from "./Start";
-
+import styled from "styled-components";
 
 function App() {
-  const [name, setName] = React.useState("르탄이");  
-
+  const ref = useRef();
+  const [name, setName] = useState('르탄이');
+  const setValue = () => {
+    const val = ref.current.value
+    setName(val)
+  }
 
   return (
-    <div className="App" style={{
-      maxWidth: "350px",
-      margin: "auto"
-    }}>
+    <Wraper className="App">
       <Start name={name}/>
-    </div>
+      <TextWrit>
+        <Input type="text" ref={ref}/>
+        <Button onClick={setValue}>시작하기</Button>
+      </TextWrit>
+    </Wraper>
   );
 }
+
+const Wraper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`
+
+const TextWrit = styled.div`
+  width: auto;
+  margin-top: 50px;
+`
+const Input = styled.input`
+  border: 1px solid #dadafc;
+  border-radius: 30px;
+  padding: 10px;
+`
+const Button = styled.button`
+  padding: 10px 36px;
+  border-color: #dadafc;
+  display: block;
+  border: none;
+  background-color: #dadafc;
+  border-radius: 30px;
+  margin: 26px auto 0 auto;
+`
 
 export default App;
--- sparta_react/work/src/Start.js
+++ sparta_react/work/src/Start.js
@@ -1,42 +1,22 @@
-import React from "react";
+import React from 'react';
+import styled from "styled-components";
 
-const Start = (props) => {
-    console.log(props);
+function Start({name}) {
+
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100vh",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: "16px",
-        boxSizing: "border-box",
-      }}
-    >
-     
-      <h1 style={{ fontSize: "1.5em", lineHeight: "1.5" }}>
-        나는 <span style={{
-            backgroundColor: "#fef5d4",
-            padding: "5px 10px",
-            borderRadius: "30px"
-        }}>{props.name}</span>에 대해 얼마나 알고 있을까?
-      </h1>
-      <input style={{
-          border: "1px solid #dadafc",
-          borderRadius: "30px",
-          padding: "10px", 
-          width: "100%"
-      }}/>
-      <button style={{
-          padding: "10px 36px",
-          backgroundColor: "#dadafc",
-          border: "#dadafc",
-          borderRadius: "30px",
-          margin: "36px 0px"
-      }}>시작하기</button>
-    </div>
+    <TextPrint>나는 {name ? <Text>{name}</Text> : null} 에 대해서<br/> 얼마나 알고 있을까?</TextPrint>
   );
-};
+}
 
+
+const TextPrint = styled.div`
+  height: 30px;
+  line-height: 30px;
+`
+
+const Text = styled.span`
+  background-color: #fef5d4;
+  padding: 5px 10px;
+  border-radius: 30px;
+`
 export default Start;
