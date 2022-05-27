bdc9a9cd2379a8fb161e8f3bd7279919bbcf104e
younhoso <thdbsgh3443@naver.com>
2022-05-27 10:02:51 +0900

    오늘을 기준으로 요일을 보여주기 기능 추가

--- sparta_react/homework/src/App.js
+++ sparta_react/homework/src/App.js
@@ -8,13 +8,27 @@ function random(n) {
   return Math.ceil(Math.random() * n);
 }
 
+const day_dict = [
+  {id: 1, nowday: "일"},
+  {id: 2, nowday: "월"},
+  {id: 3, nowday: "화"},
+  {id: 4, nowday: "수"},
+  {id: 5, nowday: "목"},
+  {id: 6, nowday: "금"},
+  {id: 7, nowday: "토"},
+]
+
 function App() {
-  const [list, setList] = useState(["월", "화", "수", "목", "금", "토", "일"]);
+  const [list, setList] = useState(day_dict);
   
   const [items, setItems] = useState(0);
-  const week_rates = list.map((day, idx) => {
+  const week_rates = list.map((daynow, idx) => {
+    const today = new Date().getDay();
+    const day = today + idx > 6 //오늘을 기준으로 요일을 보여주기
+      ? today + idx - 7
+      : today + idx
     return {
-      day: day,
+      day: day_dict[day].nowday,
       rate: random(5),
     }
   });
