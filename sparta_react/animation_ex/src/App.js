f999bd22954b1e0a7241490dd488b846473ac47f
younhoso <thdbsgh3443@naver.com>
2022-05-27 14:53:10 +0900

    keyframes

--- sparta_react/animation_ex/src/App.js
+++ sparta_react/animation_ex/src/App.js
@@ -1,8 +1,37 @@
+import styled, { keyframes } from "styled-components";
+
 function App() {
   return (
     <div className="App">
+      <Box />
     </div>
   );
 }
+
+const boxAnimation = keyframes`
+  0% {
+    border-radius: 0px;
+    top: 400px;
+  }
+  50% {
+    border-radius: 50px;
+    top: 700px;
+  }
+  100% {
+    border-radius: 0px;
+    top: 30px;
+  }
+`
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: green;
+  border-radius: 0;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  animation: ${boxAnimation} 2s 1s infinite linear;
+`
 
 export default App;
