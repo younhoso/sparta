56f2070a2e3260b9b2f594175dc92ec2b118b927
younhoso <thdbsgh3443@naver.com>
2022-05-28 12:59:47 +0900

    수정

--- sparta_react/redux/3weeks_homework/src/redux/configStore.js
+++
@@ -1,8 +0,0 @@
-import {legacy_createStore as createStore, combineReducers} from "redux";
-import quiz from "./modules/quiz";
-import user from './modules/user';
-
-const rootReducer = combineReducers({ quiz, user });
-const store = createStore(rootReducer);
-
-export default store;
