09efe77042acf404a265de888165d9d6c0fe6cf8
younhoso <thdbsgh3443@naver.com>
2022-06-06 22:27:28 +0900

    3-3 xhr.readyState

--- sparta_react/deep_react/3_w_network_ex/src/App.js
+++ sparta_react/deep_react/3_w_network_ex/src/App.js
@@ -1,4 +1,23 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:5001/sleep_times");
+    xhr.send();
+
+    // XMLHttpRequest.readyState
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState === 4) {
+        console.log(xhr.responseText);
+      }
+    }
+
+    xhr.onload = function() {
+      console.log(xhr.responseText);
+    }
+  },[]);
+
   return (
     <div className="App">
       
